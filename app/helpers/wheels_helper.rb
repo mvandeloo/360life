4bcda3df94fdf6cc96287797e04fcd0b1467d7c2
Deniss <den.b@itservicesgroup.co.uk>
2013-11-04 20:08:31 +0000

    static arcs from d3

--- app/helpers/wheels_helper.rb
+++ app/helpers/wheels_helper.rb
@@ -1,27 +1,26 @@
 module WheelsHelper
-	def nest(count, hash, name, size="1")
-		nested = hash
-		1.upto(count) do |n|
-			opts = {name: name, size: size}
-			opts[:children] = [nested] unless nested.empty?
+	# def nest(count, hash, name, size="1")
+	# 	nested = hash
+	# 	1.upto(count) do |n|
+	# 		opts = {name: name, size: size}
+	# 		opts[:children] = [nested] unless nested.empty?
 
-			nested = hash.merge(opts)
-		end
+	# 		nested = hash.merge(opts)
+	# 	end
 
-		return nested	
-	end
+	# 	return nested	
+	# end
 
 
 	def nesting_for_sections(wheel)
 		children = []
 		max_score = 20;
-		children << nest(max_score, {}, "referance", "0.01")
 
 		wheel.answers.group_by(&:section).each do |section, answers|
-			children << nest(answers.map(&:answer_value).sum, {}, section.section_name)
+			children << {name: section.section_name, size: answers.map(&:answer_value).sum}
 		end
 
-		{ name: 'Wheel of life', children: children }
+		{ name: 'Wheel of life', size:"1", children: children }
 	end
 	
 end
--- vendor/assets/javascripts/z-sanburst-d3.js
+++ vendor/assets/javascripts/z-sanburst-d3.js
@@ -11,53 +11,55 @@ var y = d3.scale.linear()
 
 var color = d3.scale.category20c();
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#sunburst").append("svg")
     .attr("width", width)
     .attr("height", height)
     .append("g")
     .attr("transform", "translate(" + width / 2 + "," + (height / 2) + ")");
 
 var partition = d3.layout.partition()
-    .value(function(d) { return d.size; });
+    .value(function(d) { return 1; });
 
 var arc = d3.svg.arc()
     .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x))); })
     .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx))); })
-    .innerRadius(function(d) { return Math.max(0, y(d.y)); })
-    .outerRadius(function(d) { return Math.max(0, y(d.y + d.dy)); });
+    .innerRadius(function(d) { return 0; })
+    .outerRadius(function(d) { return Math.max(0, y(d.size)/20); });
 
 window.count = 1;
 
 d3.json(window.location + ".json", function(error, root) {
 
   var path = svg.selectAll("path")
-      .data(partition.nodes(root))
+    .data(partition.nodes(root))
     .enter().append("path")
-      .attr("d", arc)
-      .attr("stroke-width",0.3)
-      // , function(d) { window.count++; return (window.count % 2) * 2 }
-      .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
-      .on("click", click);
+    .attr("d", arc)
+    .attr("stroke-width",0.3)
+    // , function(d) { window.count++; return (window.count % 2) * 2 }
+    .attr("fill", function(d) { return color(d.name); })
+    .attr("class", "wheel_arc")
+    .on("click", click);
 
-  function click(d) {
-    console.log(d.name);
+  var g = svg.selectAll("g")
+    .data(partition.nodes(root))
+    .enter().append("g");
 
-  //alert("You chose:" +d.name);
-    path.transition()
-      .duration(2050)
-      .attrTween("d", arcTween(inner_child(d)));
+  var text = g.append("text")
+    .attr("x", function(d) { return x(d.x); })
+    .attr("dx", "60") // margin
+    .attr("dy", ".35em") // vertical-align
+    .text(function(d) { return d.name; });
+
+
+
+  function computeTextRotation(d) {
+    var angle = x(d.x + d.dx / 2) - Math.PI / 2;
+    return angle / Math.PI * 180;
   }
+  // text.attr("transform", function(d) { return "rotate(" + computeTextRotation(d) + ")"; });
+
 });
 
-function inner_child(d){
-  var node = d;
-  var child_of_node = d;
-  while(node.parent){
-    child_of_node = node;
-    node = node.parent;
-  }
-  return child_of_node;
-}
 
 d3.select(self.frameElement).style("height", height + "px");
 

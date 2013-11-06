96f837ea78f275ab0e780ddb065180e8214afeaa
Lorraine Phillips <lp3767-web@yahoo.com>
2013-11-05 18:20:25 -0800

    formatted error message

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ LifeApp::Application.routes.draw do
 
   root :to => "static#homepage"
 
-  get '/about' => 'static#about'
+  # get '/about' => 'static#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

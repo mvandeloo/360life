2d36218263abbcdf4fe135fb94473dbe23afb159
Deniss <den.b@itservicesgroup.co.uk>
2013-11-06 09:53:37 +0000

    wheel correctly displayed and aligned and modal msg popups

--- app/helpers/wheels_helper.rb
+++ app/helpers/wheels_helper.rb
@@ -1,5 +1,5 @@
 module WheelsHelper
-	def nest(count, hash, name, section_id,size="1")
+	def nest(count, hash, name,size="1",section_id)
 		nested = hash
 		1.upto(count) do |n|
 			opts = {name: name, size: size, id: section_id}
@@ -15,7 +15,7 @@ module WheelsHelper
 	def nesting_for_sections(wheel)
 		children = []
 		max_score = 20;
-		children << nest(max_score, {}, "referance", "0.01")
+		children << nest(max_score, {}, "referance", "0.01"," ")
 
 		wheel.answers.group_by(&:section).each do |section, answers|
 			children << nest(answers.map(&:answer_value).sum, {}, section.section_name, section.id)
